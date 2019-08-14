17088e27376b99e530310be71de698843caaa5e3
Ankit Arun <ankit.arun@getpostman.com>
2019-08-14 17:47:19 +0530

    CHAT-APP-03 added basic message sending functionality

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
+  console.log('a user connected');
+  socket.broadcast.emit('hi');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
+
+   socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
